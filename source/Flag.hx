a6d6780265d5b1e8959c14566161efc914a50561
Unknown <smithn10@rpi.edu>
2018-09-23 19:23:25 -0400

    Uncommented out functionality of flag

--- source/Flag.hx
+++ source/Flag.hx
@@ -14,15 +14,17 @@ class Flag extends FlxSprite {
         super(X, Y, SimpleGraphic); // add in flag image when complete
     }
 
-    /*public function pickUp(player:Player, X:Float, Y:Float) {
+    public function pickUp(player:Player, X:Float, Y:Float) {
         // character picks up flag at (X, Y)
         player.hasFlag = true;
+        //player.toggleFlag();
     }
 
     public function flagSteal(player1:Player, other:Player) {
         // player who had flag gets stunned and flag goes to opponent
         player1.hasFlag = false;
-        player1.stun(); // or player1.respawn();
+        player1.stun();
         other.hasFlag = true;
-    }*/
+        //other.toggleFlag();
+    }
 }
