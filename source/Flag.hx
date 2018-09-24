208ad259d8763cfe52ebc3b2444be55e970cf127
Unknown <smithn10@rpi.edu>
2018-09-24 17:05:07 -0400

    Added Cutscene State, added method to LevelParser, added test files for other two levels and test images for the level screenshots

--- source/Flag.hx
+++ source/Flag.hx
@@ -14,7 +14,7 @@ class Flag extends FlxSprite {
         super(X, Y, SimpleGraphic); // add in flag image when complete
     }
 
-    public function pickUp(player:Player, X:Float, Y:Float) {
+    /*public function pickUp(player:Player, X:Float, Y:Float) {
         // character picks up flag at (X, Y)
         player.hasFlag = true;
         //player.toggleFlag();
@@ -26,5 +26,5 @@ class Flag extends FlxSprite {
         player1.stun();
         other.hasFlag = true;
         //other.toggleFlag();
-    }
+    }*/
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,19 +8,38 @@ import flixel.util.FlxColor;
 
 class PlayState extends FlxState {
 	var stateInfo:FlxText;
-	var selectB:FlxButton;
+	var level1:FlxButton;
+	var level2:FlxButton;
+	var level3:FlxButton;
+	var parser:LevelParser = new LevelParser();
+	var levelScreenshots:Array<String> = new Array<String>();
 
 	override public function create():Void {
+		var screenshot1:String = parser.getScreenshot("assets/data/testLevelSelect.txt");
+		var screenshot2:String = parser.getScreenshot("assets/data/testScreenshot2.txt");
+		var screenshot3:String = parser.getScreenshot("assets/data/testScreenshot3.txt");
+		levelScreenshots.push(screenshot1);
+		levelScreenshots.push(screenshot2);
+		levelScreenshots.push(screenshot3);
+
 		stateInfo = new FlxText(10, 30, 150);
         stateInfo.text = "Level Select\nState";
         stateInfo.setFormat("assets/font.ttf", 20, FlxColor.WHITE, CENTER);
 		stateInfo.setBorderStyle(OUTLINE, FlxColor.ORANGE, 1);
 
-		selectB = new FlxButton(500, 620, "Select", clickSelect); // (x, y, text, function call on button click)
-        selectB.label.setFormat("assets/font.ttf", 10, FlxColor.WHITE, CENTER);
-        selectB.label.setBorderStyle(OUTLINE, FlxColor.BLUE, 1);
 
-		add(selectB);
+		level1 = new FlxButton(50, 100, "", click1);
+		level1.loadGraphic(levelScreenshots[0], true, 260, 160);
+
+		level2 = new FlxButton(400, 400, "", click2);
+		level2.loadGraphic(levelScreenshots[1], true, 260, 160);
+
+		level3 = new FlxButton(750, 100, "", click3);
+		level3.loadGraphic(levelScreenshots[2], true, 260, 160);
+
+		add(level1);
+		add(level2);
+		add(level3);
 		add(stateInfo);
 	}
 
@@ -31,7 +50,15 @@ class PlayState extends FlxState {
 		super.update(elapsed);
 	}
 
-	function clickSelect():Void {
-		FlxG.switchState(new LevelState());
+	function click1():Void {
+		FlxG.switchState(new CutsceneState("assets/data/testLevelSelect.txt"));
+	}
+
+	function click2():Void {
+		FlxG.switchState(new CutsceneState("assets/data/testScreenshot2.txt"));
+	}
+
+	function click3():Void {
+		FlxG.switchState(new CutsceneState("assets/data/testScreenshot3.txt"));
 	}
 }
